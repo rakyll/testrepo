a111be76f6da7fda0333894dbfb1dd62f49eff6e
Jaana Burcu Dogan <jbd@google.com>
2017-09-20 23:57:12 -0700

    reduce sleep time

--- main_test.go
+++ main_test.go
@@ -6,24 +6,31 @@ import (
 )
 
 func TestA(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 }
 
 func TestB(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 }
 
 func TestC(t *testing.T) {
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Millisecond)
+	t.Error("error: cannot open a new connection")
 	t.Fatal("failed")
 }
 
 func TestD(t *testing.T) {
-	time.Sleep(2 * time.Second)
+	t.Parallel()
+	time.Sleep(200 * time.Millisecond)
 }
 
+func TestDD(t *testing.T) {
+	t.Parallel()
+	time.Sleep(500 * time.Millisecond)
+}
 func TestE(t *testing.T) {
 	t.Run("F", func(t *testing.T) {
+		t.Error("error: cannot open a new connection")
 	})
-	time.Sleep(1 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 }
